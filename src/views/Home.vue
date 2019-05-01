9656eaba0ae7c683a33241d32c5b08418bf18cf5
ad665 <ad665@cornell.edu>
2019-05-01 17:54:10 -0400

    download

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,8 @@
     <br>
     <p>You indicated that {{text}} people are in the gym. Press submit if this is correct.</p>
     <br>
-    <button v-on:click="handler">Submit</button>
+    <button v-on:click="handler">Submit</button> <br> 
+    <button v-on:click="download">Download Gym Data</button>
   </div>
 </template>
 
@@ -17,6 +18,7 @@
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 import * as firebase from 'firebase';
+import * as axios from 'axios'; 
 // import "firebase/auth";
 
 export default {
@@ -52,6 +54,29 @@ export default {
         console.log('signed out')
         // TODO add a procedure to redirect back to login just in case (this is already handled in router but be safe)
       })
+    },
+    download() {
+      const url = 'https://us-central1-campus-density-backend.cloudfunctions.net/getData?id=teagle';
+      axios.get(url, {mode: 'no-cors'})
+      .then((res) => {
+        console.log(res.data); 
+        const storage = firebase.storage(); 
+        const gsref = storage.refFromURL(`gs:/${res.data}/teagle.xlsx/`); 
+        gsref.getDownloadURL().then(url => {
+          window.open(url); 
+          // const xhr = new XMLHttpRequest(); 
+          // xhr.responseType = 'blob'; 
+          // xhr.onload = function(event) {
+          //   const blob = xhr.response; 
+          // }
+          // xhr.open('GET', url); 
+          // xhr.send(); 
+        })
+      })
+      .catch((err) => {
+        console.log(err); 
+      })
+
     }
   }
 };
