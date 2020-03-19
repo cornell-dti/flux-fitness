8f203e065da925087b2da41ef17bf87b279057bb
Andrew <kungpaogao@gmail.com>
2020-03-19 02:05:49 -0400

    Removed unnecessary div, added action color option

--- src/components/TopActions.vue
+++ src/components/TopActions.vue
@@ -1,11 +1,17 @@
 <template>
   <v-row>
-    <div v-for="act in actions" :key="act.desc">
-      <v-btn class="mr-1" rounded text @click="$emit(act.emit)">
-        <v-icon left>{{ act.icon }}</v-icon>
-        {{ act.desc }}
-      </v-btn>
-    </div>
+    <v-btn
+      v-for="act in actions"
+      :key="act.desc"
+      class="mr-1"
+      rounded
+      text
+      @click="$emit(act.emit)"
+      :color="act.color"
+    >
+      <v-icon left>{{ act.icon }}</v-icon>
+      {{ act.desc }}
+    </v-btn>
   </v-row>
 </template>
 
@@ -15,7 +21,7 @@ import Component from "vue-class-component";
 
 @Component({})
 export default class TopActions extends Vue {
-  actions: { desc: string; icon: string; emit: string }[] = [
+  actions: { desc: string; icon: string; emit: string; color?: string }[] = [
     { desc: "Export", icon: "cloud_download", emit: "export" },
     { desc: "Help", icon: "help", emit: "help" },
     { desc: "Log Out", icon: "exit_to_app", emit: "logout" }
