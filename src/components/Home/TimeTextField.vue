4eb6fb8c85109517cbc2c0c3604187b607f7b5da
Andrew <kungpaogao@gmail.com>
2020-04-25 01:46:02 -0400

    Implemented TimeTextField component
    - cleaner input for time
    - should be more compatible
    - more customizable
    - includes validation

---
+++ src/components/Home/TimeTextField.vue
@@ -0,0 +1,43 @@
+<template>
+<v-col class="ma-0 pa-0" cols="6">
+
+  <v-text-field
+    :value="value"
+    :rules="rules"
+    :maxlength="inputCharLimit"
+    :clearable="clearable"
+    @input="$emit('input', $event)"
+  >
+    <div class="d-flex align-center" slot="prepend">
+      <v-progress-circular :rotate="-90" :size="25" :width="3" :value="seconds * 5/3">
+        <v-icon :size="20" color="black">schedule</v-icon>
+      </v-progress-circular>
+    </div>
+  </v-text-field>
+</v-col>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+import { Prop } from "vue-property-decorator";
+
+@Component({})
+export default class TimeTextField extends Vue {
+  @Prop()
+  readonly value!: string;
+
+  @Prop()
+  readonly seconds!: number;
+
+  readonly inputCharLimit = 8;
+  readonly rules = [
+    (v: any) => !!v || "This field is required",
+    (v: any) =>
+      (v && v.length <= this.inputCharLimit) ||
+      "Input is over the character limit",
+    (v: any) => (v && /^([0-1]?[0-9]|2[0-3]):([0-5][0-9]) (AM|PM)$/i.test(v)) || "Please enter a valid time: hh:mm AM/PM",
+  ];
+  readonly clearable = false;
+}
+</script>
