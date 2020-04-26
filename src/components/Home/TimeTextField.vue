f843f33c41d4b75812e5bd9a2afcc95c7522e3ce
Andrew <kungpaogao@gmail.com>
2020-04-26 00:00:50 -0400

    Implemented better validation for TimeTextField
    - validates time and time diff (should be within 30 minutes)
    - uses Moment
    - doesn't emit unless validated

--- src/components/Home/TimeTextField.vue
+++ src/components/Home/TimeTextField.vue
@@ -5,7 +5,7 @@
       :rules="rules"
       :maxlength="inputCharLimit"
       :clearable="clearable"
-      @input="$emit('input', $event)"
+      @input="updateTime($event)"
     >
       <div class="d-flex align-center" slot="prepend">
         <v-progress-circular
@@ -33,6 +33,7 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 import { Prop } from "vue-property-decorator";
+import moment from "moment";
 
 @Component({})
 export default class TimeTextField extends Vue {
@@ -51,10 +52,38 @@ export default class TimeTextField extends Vue {
     (v: any) =>
       (v && v.length <= this.inputCharLimit) ||
       "Input is over the character limit",
+    (v: any) => this.validTime(v) || "Please enter a valid time",
     (v: any) =>
-      (v && /^([0-1]?[0-9]|2[0-3]):([0-5][0-9]) (AM|PM)$/i.test(v)) ||
-      "Please enter a valid time: hh:mm AM/PM",
+      this.validTimeDiff(v) ||
+      "Only times within the past 30 minutes are valid",
   ];
   readonly clearable = false;
+
+  updateTime(value: string): void {
+    if (this.validTime(value) && this.validTimeDiff(value)) {
+      this.$emit("input", value);
+    }
+  }
+
+  validTime(value: any): boolean {
+    return moment(
+      `${moment().format("YYYY-MM-DD")} ${value}`,
+      ["YYYY-MM-DD h:mm A", "YYYY-MM-DD hh:mm A", "YYYY-MM-DD HH:mm"],
+      true
+    ).isValid();
+  }
+
+  validTimeDiff(value: any): boolean {
+    const momentTime = moment(
+      `${moment().format("YYYY-MM-DD")} ${value.toLowerCase()}`,
+      ["YYYY-MM-DD h:mm A", "YYYY-MM-DD hh:mm A", "YYYY-MM-DD HH:mm"],
+      true
+    );
+    if (momentTime.isValid()) {
+      const diff = moment().diff(momentTime, "minutes");
+      return diff <= 30 && diff >= 0;
+    }
+    return false;
+  }
 }
 </script>
