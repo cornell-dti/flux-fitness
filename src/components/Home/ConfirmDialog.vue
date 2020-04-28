9d4e616647338e8089ed9b7481fc112616d1b9d7
Andrew <kungpaogao@gmail.com>
2020-04-28 16:11:47 -0400

    Implemented initial layout for new confirm dialog

--- src/components/Home/ConfirmDialog.vue
+++ src/components/Home/ConfirmDialog.vue
@@ -1,8 +1,47 @@
 <template>
-  <v-dialog :value="value" max-width="350" @click:outside="exitDialog()">
+  <v-dialog :value="value" max-width="500" @click:outside="exitDialog()">
     <v-card>
-      <v-card-title>Confirm Submission</v-card-title>
-      <v-card-text>{{ confirm }}</v-card-text>
+      <v-card-title>Please confirm</v-card-title>
+      <v-card-text>
+        <v-row>
+          <v-col>
+            <h4>Apr 28 2020 | 16:07</h4>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="5">
+            <h2>Cardio</h2>
+            <p><b>Total:</b> 0</p>
+          </v-col>
+          <v-col>
+            <p>Treadmills: 0</p>
+            <p>Ellipticals: 0</p>
+            <p>Bikes: 0</p>
+            <p>AMTs: 0</p>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="5">
+            <h2>Weights</h2>
+            <p><b>Total:</b> 0</p>
+          </v-col>
+          <v-col>
+            <p>Power Racks: 0</p>
+            <p>Benchpress: 0</p>
+            <p>Dumbbells: 0</p>
+            <p>Other: 0</p>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="5">
+            <h2>Total</h2>
+          </v-col>
+          <v-col>
+            <p>Noyes: 0</p>
+          </v-col>
+        </v-row>
+      </v-card-text>
+      <!-- <v-card-text>{{ confirm }}</v-card-text> -->
       <v-card-actions>
         <v-spacer />
         <v-btn text @click="exitDialog()">Edit</v-btn>
